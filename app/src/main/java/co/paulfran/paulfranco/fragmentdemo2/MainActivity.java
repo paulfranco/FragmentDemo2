f33a4078a3545b1589016db392f7f8abbb8adc20
Paul Franco <paulfranco@me.com>
2018-07-01 22:25:13 -0700

    Initial project setup

---
+++ app/src/main/java/co/paulfran/paulfranco/fragmentdemo2/MainActivity.java
@@ -0,0 +1,26 @@
+package co.paulfran.paulfranco.fragmentdemo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addFragmentA(View view) {
+    }
+
+    public void removeFragmentA(View view) {
+    }
+
+    public void addFragmentB(View view) {
+    }
+
+    public void remFragmentB(View view) {
+    }
+}
